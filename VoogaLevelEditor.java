5253c8a67c6f804feaddcbe9a042d24b6d5ba1cf
Michael Zhou <zhoumotongxue008@gmail.com>
2012-04-02 04:45:37 -0400

    Run SimpleGameToTestLevelEditor for a test

--- VoogaLevelEditor.java
+++ VoogaLevelEditor.java
@@ -1,3 +1,6 @@
+/**
+ * @author Michael Zhou (Dominator008)
+ */
 package leveleditor;
 
 import game.SpriteWrapper;
--- VoogaLevelEditorModel.java
+++ VoogaLevelEditorModel.java
@@ -1,3 +1,6 @@
+/**
+ * @author Michael Zhou (Dominator008)
+ */
 package leveleditor;
 import game.LevelState;
 import game.SpriteWrapper;
