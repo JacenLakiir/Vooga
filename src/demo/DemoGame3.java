fb9a66a24bbcd39ea0d3bfa34485918a92c6fb97
Kathleen <kathleen.oshima@gmail.com>
2012-04-06 13:44:13 -0400

    added to demogame3

--- src/demo/DemoGame3.java
+++ src/demo/DemoGame3.java
@@ -6,6 +6,9 @@ package demo;
 
 import game.AdvancedPlayField;
 import game.KeepLeftFirstPlayerGameScroller;
+import items.CollectibleInventoryItem;
+import items.CollectibleItem;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
@@ -20,70 +23,73 @@ import mario.Mario;
 
 import charactersprites.Player;
 import collision.CharacterPlatformCollision;
+import collision.PlayerCollectibleItemCollision;
 
 import com.golden.gamedev.Game;
 import com.golden.gamedev.GameLoader;
 import com.golden.gamedev.object.background.ColorBackground;
 
 public class DemoGame3 extends Game {
-	
-    private AdvancedPlayField myPlayfield;
-	
-    public void initResources ()
-    {
-    	// Playfield Init
-    	myPlayfield = new AdvancedPlayField(10000, 500);
-    	myPlayfield.setBackground(new ColorBackground(Color.gray));
-    	myPlayfield.setGameScroller(new KeepLeftFirstPlayerGameScroller());
 
-        // Collisions
-        myPlayfield.addCollisionGroup(myPlayfield.getPlayers(),
-                                      myPlayfield.getSetting(),
-                                      new CharacterPlatformCollision());
-        
-        // Sprite Init / Or load funcitonality
-        
-        Player temp = new Mario(this);
-        temp.setKeyList(new KeyConfig(temp,this).getInputKeyList());
-        temp.setImages(this.getImages("resources/Mario1.png", 1, 1));
-        temp.setLocation(25, 20);
-        myPlayfield.addPlayer(temp);
-        
-        Platform temp1 = new BasePlatform(this);
-        temp1.setImages(this.getImages("resources/Bar.png", 1, 1));
-        temp1.setLocation(0, 440);
-        myPlayfield.addSetting(temp1);
-        
-        Platform temp2 = new BasePlatform(this);
-        temp2.setImages(this.getImages("resources/Bar.png", 1, 1));
-        temp2.setLocation(600, 440);
-        myPlayfield.addSetting(temp2);
-        
-        Platform block2 = new BreakableDecorator(new BasePlatform(this));
-        block2.setMass(6);
-        block2.setMovable(false);
-        block2.setImages(this.getImages("resources/Block2.png", 1, 1));
-        block2.setLocation(160, 200);
-        myPlayfield.addSetting(block2);
+	private AdvancedPlayField myPlayfield;
 
-    }
+	public void initResources() {
+		// Playfield Init
+		myPlayfield = new AdvancedPlayField(10000, 500);
+		myPlayfield.setBackground(new ColorBackground(Color.gray));
+		myPlayfield.setGameScroller(new KeepLeftFirstPlayerGameScroller());
 
+		// Collisions
+		myPlayfield.addCollisionGroup(myPlayfield.getPlayers(),
+		        myPlayfield.getSetting(), new CharacterPlatformCollision());
 
-    public void update (long arg0)
-    {
-        myPlayfield.update(arg0);
-    }
+		myPlayfield.addCollisionGroup(myPlayfield.getPlayers(),
+		        myPlayfield.getItems(), new PlayerCollectibleItemCollision());
 
+		// Sprite Init / Or load funcitonality
 
-    public void render (Graphics2D arg0)
-    {
-        myPlayfield.render(arg0);
-    }
-    
-    public static void main(String[] args) {
-        GameLoader game = new GameLoader();
-        game.setup(new DemoGame3(), new Dimension(640,480), false);
-        game.start();
-    }
+		Player temp = new Mario(this);
+		temp.setKeyList(new KeyConfig(temp, this).getInputKeyList());
+		temp.setImages(this.getImages("resources/Mario1.png", 1, 1));
+		temp.setLocation(25, 20);
+		myPlayfield.addPlayer(temp);
+
+		Platform temp1 = new BasePlatform(this);
+		temp1.setImages(this.getImages("resources/Bar.png", 1, 1));
+		temp1.setLocation(0, 440);
+		myPlayfield.addSetting(temp1);
+
+		Platform temp2 = new BasePlatform(this);
+		temp2.setImages(this.getImages("resources/Bar.png", 1, 1));
+		temp2.setLocation(600, 440);
+		myPlayfield.addSetting(temp2);
+
+		Platform block2 = new BreakableDecorator(new BasePlatform(this));
+		block2.setMass(6);
+		block2.setMovable(false);
+		block2.setImages(this.getImages("resources/Block2.png", 1, 1));
+		block2.setLocation(160, 200);
+		myPlayfield.addSetting(block2);
+
+		CollectibleItem coin = new CollectibleInventoryItem(this);
+		coin.setImages(this.getImages("resources/Coin.png", 1, 1));
+		coin.setLocation(260, 100);
+		myPlayfield.addItem(coin);
+
+	}
+
+	public void update(long arg0) {
+		myPlayfield.update(arg0);
+	}
+
+	public void render(Graphics2D arg0) {
+		myPlayfield.render(arg0);
+	}
+
+	public static void main(String[] args) {
+		GameLoader game = new GameLoader();
+		game.setup(new DemoGame3(), new Dimension(640, 480), false);
+		game.start();
+	}
 
 }
