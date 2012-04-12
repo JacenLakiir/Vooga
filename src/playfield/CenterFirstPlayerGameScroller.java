dcb7c6bb6f2a27461d3b01334c965a7efa8f2cf4
Glenn Rivkees <glenn.rivkees@gmail.com>
2012-04-11 22:55:34 -0400

    added playfield

---
+++ src/playfield/CenterFirstPlayerGameScroller.java
@@ -0,0 +1,15 @@
+/**
+ * @author Glenn Rivkees (grivkees)
+ */
+package playfield;
+
+import com.golden.gamedev.object.Background;
+import com.golden.gamedev.object.SpriteGroup;
+
+public class CenterFirstPlayerGameScroller extends GameScroller {
+
+	public void scroll() {
+		myBackground.setToCenter(myPlayers.getActiveSprite());
+	}
+
+}
