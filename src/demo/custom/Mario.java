227d8997e427c90293609a9980d99cbc2c67eea8
Eric Mercer <ericwjm@gmail.com>
2012-04-18 22:43:20 -0400

    Fixed Mario taking damage from NPCs

--- src/demo/custom/Mario.java
+++ src/demo/custom/Mario.java
@@ -130,17 +130,17 @@ public class Mario extends Player{
 
 
     public void afterHitFromRightBy (Goomba e) {
-        updateStateValues("hitPoints", 0);
+        updateStateValues("hitPoints", -1 * getMyStateValue("hitPoints"));
         checkDead();
     }
 
     public void afterHitFromLeftBy (Goomba e) {
-        updateStateValues("hitPoints", 0);
+        updateStateValues("hitPoints", -1 * getMyStateValue("hitPoints"));
         checkDead();
     }
 
     public void afterHitFromTopBy (Goomba e) {
-        updateStateValues("hitPoints", 0);
+        updateStateValues("hitPoints", -1 * getMyStateValue("hitPoints"));
         checkDead();
     }
     
@@ -156,7 +156,7 @@ public class Mario extends Player{
     
     public void afterHitFromTopBy (Koopa k)
     {
-        updateStateValues("hitPoints", 0);
+        updateStateValues("hitPoints", -1 * getMyStateValue("hitPoints"));
         checkDead();
     }
     
@@ -164,12 +164,12 @@ public class Mario extends Player{
     {
         if (k.isInShellState() && k.getShellSpeed() != 0)
         {
-            updateStateValues("hitPoints", 0);
+            updateStateValues("hitPoints", -1 * getMyStateValue("hitPoints"));
             checkDead();
         }
         else if (!k.isInShellState())
         {
-            updateStateValues("hitPoints", 0);
+            updateStateValues("hitPoints", -1 * getMyStateValue("hitPoints"));
             checkDead();
         }
     }
