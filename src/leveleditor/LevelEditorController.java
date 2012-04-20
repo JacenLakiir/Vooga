3cec23d3760388c20ddb0c46a0c8176d15f4ff84
Michael Zhou <zhoumotongxue008@gmail.com>
2012-04-20 06:19:22 -0400

    Minor

--- src/leveleditor/LevelEditorController.java
+++ src/leveleditor/LevelEditorController.java
@@ -16,29 +16,33 @@ import levelio.SpriteWrapper;
 public class LevelEditorController {
 
     private LevelEditor myView;
-    
+
     public LevelEditorController() {
 	myView = new LevelEditor(this);
     }
-    
+
     protected void initialize(File file) {
 	LevelState levelstate = LevelState.loadLevel(file);
 	myView.getCanvas().setUpBackground(levelstate.getBackgroundImageSrc());
 	myView.loadSprites(levelstate.getSprites());
     }
-    
+
     protected void export(File file) {
 	Canvas canvas = myView.getCanvas();
-		if (canvas.getBackgroundImageSrc() == null) return;
-		List<SpriteWrapper> sprites = new ArrayList<SpriteWrapper>();
-		for (JLabel l: canvas.getLabelWrapperMap().keySet()) {
-		    SpriteWrapper tosave = canvas.getLabelWrapperMap().get(l).clone();
-		    tosave.getGameElement().setX(l.getX());
-		    tosave.getGameElement().setY(l.getY());
-		    sprites.add(tosave);
-		}
-		new LevelState(canvas.getBackgroundImageSrc(), sprites).save(file.getCanonicalPath());
-	    }
+	if (canvas.getBackgroundImageSrc() == null)
+	    return;
+	List<SpriteWrapper> sprites = new ArrayList<SpriteWrapper>();
+	for (JLabel l : canvas.getLabelWrapperMap().keySet()) {
+	    SpriteWrapper tosave = canvas.getLabelWrapperMap().get(l).clone();
+	    tosave.getGameElement().setX(l.getX());
+	    tosave.getGameElement().setY(l.getY());
+	    sprites.add(tosave);
+	}
+	try {
+	    new LevelState(canvas.getBackgroundImageSrc(), sprites).save(file
+		    .getCanonicalPath());
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
     }
-    
 }
