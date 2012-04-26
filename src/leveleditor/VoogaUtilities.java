6613656c469112c50c8f686093acc0d3eeed2dae
Michael Zhou <zhoumotongxue008@gmail.com>
2012-04-26 16:50:30 -0400

    VoogaUtilities

--- src/leveleditor/VoogaUtilities.java
+++ src/leveleditor/VoogaUtilities.java
@@ -21,92 +21,83 @@ import java.net.URL;
 import javax.swing.ImageIcon;
 import com.golden.gamedev.util.ImageUtil;
 
-
-public class VoogaUtilities
-{
-    public static BufferedImage getImageFromString (String src)
-    {
-        BufferedImage toreturn = null;
-        try
-        {
-            toreturn =
-                ImageUtil.getImage(new URL("file://" +
-                                           new File(src).getCanonicalPath()),
-                                   Color.MAGENTA);
-        }
-        catch (MalformedURLException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return toreturn;
+public class VoogaUtilities {
+    public static BufferedImage getImage(String src) {
+	BufferedImage toreturn = null;
+	try {
+	    toreturn = ImageUtil.getImage(
+		    new URL("file://" + new File(src).getCanonicalPath()),
+		    Color.MAGENTA);
+	} catch (MalformedURLException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return toreturn;
     }
 
-
-    public static BufferedImage toBufferedImage (Image image)
-    {
-        if (image instanceof BufferedImage)
-        {
-            return (BufferedImage) image;
-        }
-        image = new ImageIcon(image).getImage();
-        boolean hasAlpha = hasAlpha(image);
-        BufferedImage bimage = null;
-        GraphicsEnvironment ge =
-            GraphicsEnvironment.getLocalGraphicsEnvironment();
-        try
-        {
-            int transparency = Transparency.OPAQUE;
-            if (hasAlpha)
-            {
-                transparency = Transparency.BITMASK;
-            }
-            GraphicsDevice gs = ge.getDefaultScreenDevice();
-            GraphicsConfiguration gc = gs.getDefaultConfiguration();
-            bimage =
-                gc.createCompatibleImage(image.getWidth(null),
-                                         image.getHeight(null),
-                                         transparency);
-        }
-        catch (HeadlessException e)
-        {}
-        if (bimage == null)
-        {
-            int type = BufferedImage.TYPE_INT_RGB;
-            if (hasAlpha)
-            {
-                type = BufferedImage.TYPE_INT_ARGB;
-            }
-            bimage =
-                new BufferedImage(image.getWidth(null),
-                                  image.getHeight(null),
-                                  type);
-        }
-        Graphics g = bimage.createGraphics();
-        g.drawImage(image, 0, 0, null);
-        g.dispose();
-        return bimage;
+    public static BufferedImage[] getImages(String src, int row, int col) {
+	BufferedImage[] toreturn = null;
+	try {
+	    toreturn = ImageUtil.getImages(
+		    new URL("file://" + new File(src).getCanonicalPath()), row, col, 
+		    Color.MAGENTA);
+	} catch (MalformedURLException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return toreturn;
+    }
+    
+    public static BufferedImage toBufferedImage(Image image) {
+	if (image instanceof BufferedImage) {
+	    return (BufferedImage) image;
+	}
+	image = new ImageIcon(image).getImage();
+	boolean hasAlpha = hasAlpha(image);
+	BufferedImage bimage = null;
+	GraphicsEnvironment ge = GraphicsEnvironment
+		.getLocalGraphicsEnvironment();
+	try {
+	    int transparency = Transparency.OPAQUE;
+	    if (hasAlpha) {
+		transparency = Transparency.BITMASK;
+	    }
+	    GraphicsDevice gs = ge.getDefaultScreenDevice();
+	    GraphicsConfiguration gc = gs.getDefaultConfiguration();
+	    bimage = gc.createCompatibleImage(image.getWidth(null),
+		    image.getHeight(null), transparency);
+	} catch (HeadlessException e) {
+	}
+	if (bimage == null) {
+	    int type = BufferedImage.TYPE_INT_RGB;
+	    if (hasAlpha) {
+		type = BufferedImage.TYPE_INT_ARGB;
+	    }
+	    bimage = new BufferedImage(image.getWidth(null),
+		    image.getHeight(null), type);
+	}
+	Graphics g = bimage.createGraphics();
+	g.drawImage(image, 0, 0, null);
+	g.dispose();
+	return bimage;
     }
 
-
-    public static boolean hasAlpha (Image image)
-    {
-        if (image instanceof BufferedImage)
-        {
-            BufferedImage bimage = (BufferedImage) image;
-            return bimage.getColorModel().hasAlpha();
-        }
-        PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
-        try
-        {
-            pg.grabPixels();
-        }
-        catch (InterruptedException e)
-        {}
-        ColorModel cm = pg.getColorModel();
-        return cm.hasAlpha();
+    public static boolean hasAlpha(Image image) {
+	if (image instanceof BufferedImage) {
+	    BufferedImage bimage = (BufferedImage) image;
+	    return bimage.getColorModel().hasAlpha();
+	}
+	PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
+	try {
+	    pg.grabPixels();
+	} catch (InterruptedException e) {
+	}
+	ColorModel cm = pg.getColorModel();
+	return cm.hasAlpha();
     }
+    
+    
+    
 }
