702aa0fa43f523e7e2710aace752e76633810d04
di <daedalusinfinity@gmail.com>
2012-04-26 10:56:37 -0700

    Making all the necessary stuff serialisable.

--- src/core/gamestate/GameEngine2D.java
+++ src/core/gamestate/GameEngine2D.java
@@ -3,48 +3,63 @@ package core.gamestate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import com.golden.gamedev.GameEngine;
 import com.golden.gamedev.GameObject;
 
-public abstract class GameEngine2D extends GameEngine {
+
+public abstract class GameEngine2D extends GameEngine
+{
     private boolean isInitial = true;
 
-    protected boolean isInitial() {
+
+    protected boolean isInitial ()
+    {
         return isInitial;
     }
 
-    protected void setInitial(boolean isInitial) {
+
+    protected void setInitial (boolean isInitial)
+    {
         this.isInitial = isInitial;
     }
 
     private List<GameObject> list = new ArrayList<GameObject>();
 
-    protected void addGameObject(GameObject object) {
+
+    protected void addGameObject (GameObject object)
+    {
         list.add(object);
     }
 
-    public abstract void registerGameObjects();
+
+    public abstract void registerGameObjects ();
 
     private HashMap<String, GameObject> map = new HashMap<String, GameObject>();
     private HashMap<String, Integer> idMap = new HashMap<String, Integer>();
 
-    protected HashMap<String, Integer> getIdMap() {
+
+    protected HashMap<String, Integer> getIdMap ()
+    {
         return idMap;
     }
 
     private int previousGameID;
 
-    protected int getPreviousGameID() {
+
+    protected int getPreviousGameID ()
+    {
         return previousGameID;
     }
 
+
     @Override
-    public void initResources() {
+    public void initResources ()
+    {
         super.initResources();
         registerGameObjects();
         int id = 0;
-        for (GameObject object : list) {
+        for (GameObject object : list)
+        {
             String name = object.getClass().getName();
             map.put(name, object);
             idMap.put(name, id);
@@ -52,34 +67,47 @@ public abstract class GameEngine2D extends GameEngine {
         }
     }
 
-    public void initializeGameList() {
+
+    public void initializeGameList ()
+    {
         list = new ArrayList<GameObject>();
     }
 
-    public Integer getGameID(String className) {
+
+    public Integer getGameID (String className)
+    {
         return idMap.get(className);
     }
 
-    protected void setInitialGameObject(Class<? extends GameObject2D> gameClass) {
+
+    protected void setInitialGameObject (Class<? extends GameObject2D> gameClass)
+    {
         nextGameID = idMap.get(gameClass.getName());
 
     }
 
+
     @Override
-    public GameObject getGame(int gameID) {
+    public GameObject getGame (int gameID)
+    {
         String className = "";
-        for (String name : idMap.keySet()) {
-            if (nextGameID == idMap.get(name))
-                className = name;
+        for (String name : idMap.keySet())
+        {
+            if (nextGameID == idMap.get(name)) className = name;
         }
         return map.get(className);
     }
 
-    public void storeCurrentGameID(Class<? extends GameObject2D> mclass) {
+
+    public void storeCurrentGameID (Class<? extends GameObject2D> mclass)
+    {
         previousGameID = idMap.get(mclass.getName());
     }
 
-    public void saveGame() {
+
+    public void saveGame ()
+    {
+        ((GameObject2D) getGame(nextGameID)).saveGame();
     }
 
 }
--- src/core/playfield/hud/HUD.java
+++ src/core/playfield/hud/HUD.java
@@ -3,43 +3,57 @@ package core.playfield.hud;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.io.Serializable;
 import java.util.ArrayList;
-
 import com.golden.gamedev.object.GameFont;
 import com.golden.gamedev.object.font.SystemFont;
 
-public class HUD {
 
-    private GameFont font = new SystemFont(
-	    new Font("Monospaced", Font.BOLD, 12));
+public class HUD implements Serializable
+{
+    private final static long serialVersionUID = 912402285760541314L;
+    private GameFont font =
+        new SystemFont(new Font("Monospaced", Font.BOLD, 12));
     private ArrayList<HUDWidget> myWidgets = new ArrayList<HUDWidget>();
     private HUDLayoutManager layoutManager;
 
-    public HUD(HUDLayoutManager m) {
-	this.layoutManager = m;
+
+    public HUD (HUDLayoutManager m)
+    {
+        this.layoutManager = m;
     }
 
-    public void addWidget(HUDWidget w) {
-	myWidgets.add(w);
-	w.setPosition(layoutManager.nextWidgetPosition(w));
+
+    public void addWidget (HUDWidget w)
+    {
+        myWidgets.add(w);
+        w.setPosition(layoutManager.nextWidgetPosition(w));
     }
 
-    public void update(long t) {
-	for (HUDWidget w : myWidgets) {
-	    w.update(t);
-	}
+
+    public void update (long t)
+    {
+        for (HUDWidget w : myWidgets)
+        {
+            w.update(t);
+        }
     }
 
-    public void render(Graphics2D g) {
-	// g.clearRect(0,0,layoutManager.width,layoutManager.height);
-	for (HUDWidget w : myWidgets) {
-	    g.setColor(Color.black);
-	    w.render(g, this);
-	}
+
+    public void render (Graphics2D g)
+    {
+        // g.clearRect(0,0,layoutManager.width,layoutManager.height);
+        for (HUDWidget w : myWidgets)
+        {
+            g.setColor(Color.black);
+            w.render(g, this);
+        }
     }
 
-    public GameFont getFont() {
-	return font;
+
+    public GameFont getFont ()
+    {
+        return font;
     }
 
 }
