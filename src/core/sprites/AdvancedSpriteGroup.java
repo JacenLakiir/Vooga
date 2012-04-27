eb3b0f67be6647cc08e2333cb4acfe888c23e3c5
Michael Zhou <zhoumotongxue008@gmail.com>
2012-04-27 04:33:28 -0400

    Major

---
+++ src/core/sprites/AdvancedSpriteGroup.java
@@ -0,0 +1,16 @@
+package core.sprites;
+
+import java.io.Serializable;
+
+
+public class AdvancedSpriteGroup extends com.golden.gamedev.object.SpriteGroup
+    implements Serializable
+{
+    private final static long serialVersionUID = 57747157713501479L;
+
+
+    public AdvancedSpriteGroup (String name)
+    {
+        super(name);
+    }
+}
