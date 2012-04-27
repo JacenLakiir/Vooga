b1f4a1793c97df84d9ca13a09f902d3e5ddb732d
di <daedalusinfinity@gmail.com>
2012-04-26 22:08:56 -0700

    Restored AdvancedSpriteGroup

---
+++ src/core/sprites/AdvancedSpriteGroup.java
@@ -0,0 +1,16 @@
+package core.sprites;
+
+import java.io.Serializable;
+import com.golden.gamedev.object.SpriteGroup;
+
+
+public class AdvancedSpriteGroup extends SpriteGroup implements Serializable
+{
+    private final static long serialVersionUID = -5913512087419430929L;
+
+
+    public AdvancedSpriteGroup (String name)
+    {
+        super(name);
+    }
+}
